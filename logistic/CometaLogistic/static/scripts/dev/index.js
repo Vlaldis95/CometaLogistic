d715d7fe5f6dc0391084632bc7ad419cc2c64ec5
Anatoliy Chudnov <tolik.chudnov@gmail.com>
2023-09-28 10:50:34 +0300

    fix: fix header and intro

--- logistic/CometaLogistic/static/scripts/dev/index.js
+++ logistic/CometaLogistic/static/scripts/dev/index.js
@@ -1,18 +1,20 @@
 const menuBurger = document.querySelector(".menu-burger");
 const menuBurgerSpans = menuBurger.querySelectorAll(".menu-burger__item");
 const header = document.querySelector(".header");
-const headerNav = header.querySelector(".header__nav");
+const headerNavContainer = header.querySelector(".header__nav-container");
 const headerButton = header.querySelector(".header__button");
 const anchors = document.querySelectorAll('a[href*="#"]');
 const intro = document.querySelector(".intro");
 const introArrow = intro.querySelector(".intro__arrow");
 const introText = intro.querySelector(".intro__text");
 const swiper = document.querySelector(".advantages__swiper");
+const root = document.querySelector(".root");
 
 function closeHeaderMenu() {
   header.classList.remove("header_opened");
-  headerNav.classList.remove("header__nav_opened");
+  headerNavContainer.classList.remove("header__nav-container_opened");
   headerButton.classList.remove("header__button_opened");
+  root.classList.remove("root__burger_opened");
   menuBurgerSpans.forEach(function (item) {
     item.classList.remove("menu-burger__item_active");
   });
@@ -20,7 +22,8 @@ function closeHeaderMenu() {
 
 function toggleHeaderMenu() {
   header.classList.toggle("header_opened");
-  headerNav.classList.toggle("header__nav_opened");
+  root.classList.toggle("root__burger_opened");
+  headerNavContainer.classList.toggle("header__nav-container_opened");
   headerButton.classList.toggle("header__button_opened");
   menuBurgerSpans.forEach(function (item) {
     item.classList.toggle("menu-burger__item_active");
